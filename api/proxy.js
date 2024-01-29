b5287ed0b3a24b4e8c55df501591105d1adb2a54
HanBart <125204340+HanBart@users.noreply.github.com>
2024-01-29 12:57:21 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://m.youtube.com/?locale=zh_CN";//your website url
+  let target = "https://m.youtube.com/";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
